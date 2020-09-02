d303f34396b046ad1d5e2064dfdc0314bd5c8644
Jc Dior <jessicaclairdior@gmail.com>
2020-09-02 18:08:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,9 @@ def long_planeteer_calls(calls_array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if snacks.inclue?(cheese_types)
+    return true
+  end
 end
