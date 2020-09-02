830af723275f3ce419ab85bf05cde2be3c443b68
Jc Dior <jessicaclairdior@gmail.com>
2020-09-02 18:22:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,7 @@ end
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
     if snacks.include?(cheese_types)
+      return snacks.match(cheese_types)
     else return nil
     end
 end
